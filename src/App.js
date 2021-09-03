2cb60eceb8bffc1143f1a457c836a45a23a5f182
sshaikh5 <mshaikh5@gmu.edu>
2021-09-03 13:55:51 -0400

    updated App.js 3rd time

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!!--2nd time.
+          Edit <code>src/App.js</code> and save to reload!!--3rd time.
         </p>
         <a
           className="App-link"
